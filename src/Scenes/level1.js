b5e8f05362c11c0fc7656a755bec055e41c634d8
Donovan Burrola <donovan.burrola@gmail.com>
2026-06-02 20:35:35 -0700

    Added base files

---
+++ src/Scenes/level1.js
@@ -0,0 +1,18 @@
+class levelOne extends Phaser.Scene {
+    constructor() {
+        super("levelOne");
+    }
+
+    preload() {
+       this.load.setPath("./assets/");
+    }
+
+    create() {
+        
+    }
+
+    update(time, delta) {
+        
+    }
+}
+         

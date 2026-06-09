c7e5f41a54fa273ea8d18c2357e1d8e9b585ee93
Donovan Burrola <116219378+UnsafeDragoon@users.noreply.github.com>
2026-06-08 20:58:28 -0700

    Added tilemap and assets for the overworld
    Also added arcade physics and switched Canvas to AUTO!

--- src/Scenes/Overworld.js
+++ src/Scenes/Overworld.js
@@ -5,25 +5,71 @@ class overworld extends Phaser.Scene {
     create() {
 
 
-        this.map = this.add.tilemap("overworld", 16, 16, 120, 60);
+        this.map = this.make.tilemap({
+            key: "overworld"
+        });
         //this.animatedTiles.init(this.map);
-        this.tileset = this.map.addTilesetImage("monochrome_tilemap_packed", "tilemap_tiles");
-        
-        // Create a layer
-        this.groundLayer = this.map.createLayer("groundLayer", this.tileset, 0, 0);
-        this.hazardLayer = this.map.createLayer("hazardLayer", this.tileset, 16, 0);
-        this.platformLayer = this.map.createLayer("platformLayer", this.tileset, 0, 0);
+
+        // 1st param = name of tileset in TILED!      2nd param = name of image in Load.js
+        let terrainTiles = this.map.addTilesetImage("Tilemap_color2", "tilemap_terrain1");
+        let backgroundTiles = this.map.addTilesetImage("Water Background color", "tilemap_water");
+
+
+        this.bgLayer = this.map.createLayer("backgroundLayer", backgroundTiles, 0, 0);
+        this.groundLayer = this.map.createLayer("groundLayer", terrainTiles, 0, 0);
+
+
+
+
+        this.left = this.input.keyboard.addKey("A");
+        this.right = this.input.keyboard.addKey("D");
+        this.up = this.input.keyboard.addKey("W");
+        this.down = this.input.keyboard.addKey("S");
 
         
+        this.player = new Player(this, 550, 2700,  
+            "playerSprite", null,
+            this.left, this.right, this.up, this.down);
+        this.player.setScale(3);
+        this.player.moveSpeed = 600;
+        
+            
+
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.startFollow(this.player, true, 1, 1); 
+        this.cameras.main.setDeadzone(50, 50);
+        this.cameras.main.setZoom(1);
+
+
+
+        this.physics.world.setBounds(
+            0,
+            0,
+            this.map.widthInPixels,
+            this.map.heightInPixels
+        );
+
+
 
 
 
         this.input.keyboard.on('keydown-ZERO', () => {
             this.scene.start("devSpriteTest");
         });
+        this.input.keyboard.on('keydown-NINE', () => {
+            this.scene.start("battleScene");
+        });
     }
 
     update(time, delta) {
+        console.log(
+    this.player.x,
+    this.player.y,
+    this.cameras.main.scrollX,
+    this.cameras.main.scrollY
+);
+        this.player.update();
+        //console.log(this.game.loop.actualFps);
     }
 }
          
--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@
 // game config
 let config = {
     parent: 'phaser-game',
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     render: {
         pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
     },
     width: 640,         // 10 tiles, each 16 pixels, scaled 4x
     height: 640,

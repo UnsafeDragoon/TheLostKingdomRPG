8fb9a075fe1710618830e4d5cb63ea9cee0ae13f
Donovan Burrola <116219378+UnsafeDragoon@users.noreply.github.com>
2026-06-08 19:22:06 -0700

    added some assets

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -6,10 +6,24 @@ class Load extends Phaser.Scene {
     preload() {
         this.load.setPath("./assets/");
         this.load.image("playerSprite", "placeholder.png");
+
+        this.load.image("tilemap_terrain1", "Tilemap_color2.png");
+        this.load.spritesheet("tilemap_terrain_sheet1", "Tilemap_color2.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+
+        this.load.image("tilemap_water", "Water Background color.png");
+
+        
+        
+        
+        
+        this.load.tilemapTiledJSON("overworld", "overworld.tmj");
     }
 
     create() {
-    
+
         this.scene.start('overworld'); 
     }
 }
--- src/Scenes/Overworld.js
+++ src/Scenes/Overworld.js
@@ -5,6 +5,19 @@ class overworld extends Phaser.Scene {
     create() {
 
 
+        this.map = this.add.tilemap("overworld", 16, 16, 120, 60);
+        //this.animatedTiles.init(this.map);
+        this.tileset = this.map.addTilesetImage("monochrome_tilemap_packed", "tilemap_tiles");
+        
+        // Create a layer
+        this.groundLayer = this.map.createLayer("groundLayer", this.tileset, 0, 0);
+        this.hazardLayer = this.map.createLayer("hazardLayer", this.tileset, 16, 0);
+        this.platformLayer = this.map.createLayer("platformLayer", this.tileset, 0, 0);
+
+        
+
+
+
         this.input.keyboard.on('keydown-ZERO', () => {
             this.scene.start("devSpriteTest");
         });

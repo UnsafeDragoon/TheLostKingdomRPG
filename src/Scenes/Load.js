91fb9ddb345ef450f0ace84f3270e2c36907c4b8
Donovan Burrola <116219378+UnsafeDragoon@users.noreply.github.com>
2026-06-12 16:01:28 -0700

    Huge overhauls to the map and animated player obj
    Will add an animation state machine and collision for terrain next

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -7,13 +7,31 @@ class Load extends Phaser.Scene {
         this.load.setPath("./assets/");
         this.load.image("playerSprite", "placeholder.png");
 
-        this.load.image("tilemap_terrain1", "Tilemap_color2.png");
-        this.load.spritesheet("tilemap_terrain_sheet1", "Tilemap_color2.png", {
-            frameWidth: 64,
-            frameHeight: 64
+        this.load.image("tilemap_terrain1", "tiles/Tilemap_color2.png");
+       
+        this.load.spritesheet("playerIdle", "sprites/Warrior_Idle.png", {
+            frameWidth: 192,
+            frameHeight: 192
         });
 
-        this.load.image("tilemap_water", "Water Background color.png");
+        this.load.spritesheet("playerRun", "sprites/Warrior_Run.png", {
+            frameWidth: 192,
+            frameHeight: 192
+        });
+
+        this.load.image("tilemap_water", "tiles/Water Background color.png");
+        
+        this.load.image("tilemap_yMonastery", "tiles/Monastery.png");
+        this.load.image("tilemap_yHouse", "tiles/House1.png");
+        this.load.image("tilemap_yHouse2", "tiles/House2.png");
+
+        this.load.image("tilemap_bCastle", "tiles/Castle.png");
+        this.load.image("tilemap_bTower", "tiles/Tower.png");
+        this.load.image("tilemap_bArcher", "tiles/Archery.png");
+        this.load.image("tilemap_bBarracks", "tiles/Barracks.png");
+
+        this.load.image("tilemap_waterRocks", "tiles/Water Rocks_01.png");
+        this.load.image("tilemap_tree", "tiles/Tree1.png");
 
         
         
--- src/Scenes/Overworld.js
+++ src/Scenes/Overworld.js
@@ -2,21 +2,40 @@ class overworld extends Phaser.Scene {
     constructor() {
         super("overworld");
     }
+    preload(){
+        this.load.scenePlugin('AnimatedTiles', './lib/AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
+    }
     create() {
 
 
         this.map = this.make.tilemap({
             key: "overworld"
         });
-        //this.animatedTiles.init(this.map);
+        this.animatedTiles.init(this.map);
 
         // 1st param = name of tileset in TILED!      2nd param = name of image in Load.js
         let terrainTiles = this.map.addTilesetImage("Tilemap_color2", "tilemap_terrain1");
+
         let backgroundTiles = this.map.addTilesetImage("Water Background color", "tilemap_water");
 
+        let treeTiles = this.map.addTilesetImage("Tree1", "tilemap_tree");
+        let waterRockTiles = this.map.addTilesetImage("Water Rocks_01", "tilemap_waterRocks");
+        let bCastleTiles = this.map.addTilesetImage("Castle", "tilemap_bCastle");
+        let bBarrackTiles = this.map.addTilesetImage("Barracks", "tilemap_bBarracks");
+        let bArcheryTiles = this.map.addTilesetImage("Archery", "tilemap_bArcher");
+        let bTowerTiles = this.map.addTilesetImage("Tower", "tilemap_bTower");
+        let yMonasteryTiles = this.map.addTilesetImage("Monastery", "tilemap_yMonastery");
+        let yHouseTiles = this.map.addTilesetImage("House1", "tilemap_yHouse");
+        let yAltHouseTiles = this.map.addTilesetImage("House2", "tilemap_yHouse2");
+
+        let allDecor = [treeTiles, waterRockTiles, bCastleTiles,
+            bBarrackTiles, bArcheryTiles, bTowerTiles, yMonasteryTiles,
+            yHouseTiles, yAltHouseTiles
+        ];
 
         this.bgLayer = this.map.createLayer("backgroundLayer", backgroundTiles, 0, 0);
         this.groundLayer = this.map.createLayer("groundLayer", terrainTiles, 0, 0);
+        this.decorLayer = this.map.createLayer("decorLayer", allDecor, 0, 0);
 
 
 
@@ -26,12 +45,40 @@ class overworld extends Phaser.Scene {
         this.up = this.input.keyboard.addKey("W");
         this.down = this.input.keyboard.addKey("S");
 
+        //let sprite = this.add.sprite(0, 0, "playerIdle");
+
         
         this.player = new Player(this, 550, 2700,  
-            "playerSprite", null,
+            "playerIdle", 40,
             this.left, this.right, this.up, this.down);
-        this.player.setScale(3);
+        this.player.setScale(1);
         this.player.moveSpeed = 600;
+
+        this.anims.create({
+            key: "playerIdle",
+            frameRate: 12,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers("playerIdle", {
+                frames: [0, 1, 2, 3, 4, 5, 6, 7]
+            })
+        })
+        this.anims.create({
+            key: "playerRun",
+            frameRate: 12,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers("playerRun", {
+                frames: [0, 1, 2, 3, 4, 5]
+            })
+        })
+
+        this.player.play("playerIdle");
+
+
+
+
+
+
+
         
             
 
@@ -72,12 +119,12 @@ class overworld extends Phaser.Scene {
     }
 
     update(time, delta) {
-        console.log(
-    this.player.x,
-    this.player.y,
-    this.cameras.main.scrollX,
-    this.cameras.main.scrollY
-);
+//         console.log(
+//     this.player.x,
+//     this.player.y,
+//     this.cameras.main.scrollX,
+//     this.cameras.main.scrollY
+// );
         this.player.update();
         //console.log(this.game.loop.actualFps);
     }
